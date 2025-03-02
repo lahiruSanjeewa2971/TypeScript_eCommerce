4c737c1b4cd7e8d727f70669aa28c740955778be
lahiruSanjeewa2971 <rathne1997@gmail.com>
2025-03-02 17:50:05 +0530

    Project initialized | frontned part started

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>eCommerce</header>
+      <main></main>
+      <footer>All right reserved.</footer>
+    </>
+  );
+}
+
+export default App;
